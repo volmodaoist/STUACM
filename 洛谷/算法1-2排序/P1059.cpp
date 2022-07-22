985b0c2612ed00f31296ad6fc1ee0e90f34906b6
volmodaoist <1304548501@qq.com>
2022-07-22 19:01:44 +0800

    更新

---
+++ 洛谷/算法1-2排序/P1059.cpp
@@ -0,0 +1,50 @@
+#define _OJ_ONLINE_JUDGE_
+#define	min3(x,y,z)	(min(min(x,y),z))
+#define	max3(x,y,z)	(max(max(x,y),z))
+#define	CLEAR(A,x)  (memset(A,x,sizeof(A)))
+#define	ALL(x)  (x.begin()), (x.end())
+#define	INS(x)  (inserter(x, x.begin()))
+#define	INF	0x3f3f3f3f
+#define	MOD	1000000007
+#define	PI	3.1415927
+#define	EPS	1e-10
+
+
+#include <bits/stdc++.h>
+#include <limits>
+using namespace std;
+
+
+typedef long long Long;
+typedef unsigned long long ULL;
+#define  fi    first
+#define  se    second
+#define  MAXN  100005
+
+int  n, arr[MAXN];
+int main(){ 
+    #ifdef _OJ_ONLINE_JUDGE_
+    freopen("../../in.txt","r",stdin);
+    freopen("../../out.txt","w",stdout);
+    #endif
+
+    while (scanf("%d", &n) != EOF){
+        for (int i = 0; i < n; i++){
+            scanf("%d", &arr[i]);
+        }
+        sort(arr, arr + n);
+        int j = 0;
+        for (int i = 1; i < n; i++){
+            if(arr[j] < arr[i]){
+                arr[++j] = arr[i];
+            }
+        }
+        printf("%d\n", j + 1);
+        for (int i = 0; i <= j; i++){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
